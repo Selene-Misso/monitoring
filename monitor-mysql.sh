06bb973ef4406daae93e28128a31250852827d11
美代 苑生 <mishiro@dbms.cs.kumamoto-u.ac.jp>
2015-12-17 18:52:38 +0900

    1秒間隔で繰り返すように修正．

--- monitor-mysql.sh
+++ monitor-mysql.sh
@@ -6,17 +6,20 @@
 HOME=`/bin/echo $HOME`
 DIR=$HOME/data
 HOST=`/bin/hostname`
-DATE=`/bin/date "+%Y/%m/%d %H:%M:%S"`
+DATE=`/bin/date "+%Y-%m-%dT%H:%M:%SZ"`
 
 SOCKET=/var/run/mysqld/mysqld.sock
 COMMAND="/usr/bin/mysqladmin -u root -p3641ec2 extended-status"
 
 if [ -S $SOCKET ];then
-   $COMMAND | /usr/bin/awk '{print $2,$4}' | /bin/egrep -v '(^ |Variable_name)' | \
-      while read LABEL DATA
-   do
-       /bin/echo -e $DATE "\t" $DATA >> $DIR/$HOST.$LABEL
-     done
+  while true; do 
+       $COMMAND | /usr/bin/awk '{print $2,$4}' | /bin/egrep -v '(^ |Variable_name)' | \
+          while read LABEL DATA
+       do
+           /bin/echo -e $DATE "\t" $DATA >> $DIR/$HOST.$LABEL
+       done
+       sleep 1s
+  done
   else
     /bin/echo "MySQL is not running."
 fi
