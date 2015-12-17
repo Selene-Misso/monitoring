f336a9bf67786c54a86eecd235cf9c81730729c6
美代 苑生 <mishiro@dbms.cs.kumamoto-u.ac.jp>
2015-12-17 18:55:41 +0900

    時刻を1秒毎に生成するように修正．

--- monitor-mysql.sh
+++ monitor-mysql.sh
@@ -6,7 +6,7 @@
 HOME=`/bin/echo $HOME`
 DIR=$HOME/data
 HOST=`/bin/hostname`
-DATE=`/bin/date "+%Y-%m-%dT%H:%M:%SZ"`
+
 
 SOCKET=/var/run/mysqld/mysqld.sock
 COMMAND="/usr/bin/mysqladmin -u root -p3641ec2 extended-status"
@@ -16,6 +16,7 @@ if [ -S $SOCKET ];then
        $COMMAND | /usr/bin/awk '{print $2,$4}' | /bin/egrep -v '(^ |Variable_name)' | \
           while read LABEL DATA
        do
+           DATE=`/bin/date "+%Y-%m-%dT%H:%M:%SZ"`
            /bin/echo -e $DATE "\t" $DATA >> $DIR/$HOST.$LABEL
        done
        sleep 1s
