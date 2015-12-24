acc140f1deac77d88925ed7bcf6e4b0195b66d65
美代 苑生 <mishiro@dbms.cs.kumamoto-u.ac.jp>
2015-12-24 16:27:21 +0900

    時刻をISOフォーマットに変換して出力

--- convert-sar.sh
+++ convert-sar.sh
@@ -7,17 +7,17 @@
 file=sar.out
 
 # CPU
-echo "\"hostname\",\"interval\",\"timestamp\",\"CPU\",\"type\",\"value\"" > cpu.csv
-sadf -T -- -u $file |tr "\t" , >> cpu.csv
+echo "\"timestamp\",\"CPU\",\"type\",\"value\"" > cpu.csv
+sadf -T -- -u $file |awk '{print $3"T"$4"Z,"$5","$6","$7}' >> cpu.csv
 
 # Memory
-echo "\"hostname\",\"interval\",\"timestamp\",\"Memory\",\"type\",\"value\"" > memory.csv
-sadf -T -- -r $file |tr "\t" , >> memory.csv
+echo "\"timestamp\",\"Memory\",\"type\",\"value\"" > memory.csv
+sadf -T -- -r $file |awk '{print $3"T"$4"Z,"$5","$6","$7}' >> memory.csv
 
 # Network
-echo "\"hostname\",\"interval\",\"timestamp\",\"Network\",\"type\",\"value\"" > net.csv
-sadf -T -- -n DEV sar.out|grep eth0 |tr "\t" , >> net.csv
+echo "\"timestamp\",\"Network\",\"type\",\"value\"" > net.csv
+sadf -T -- -n DEV sar.out|grep eth0 |awk '{print $3"T"$4"Z,"$5","$6","$7}' >> net.csv
 
 # IO
-echo "\"hostname\",\"interval\",\"timestamp\",\"IO\",\"type\",\"value\"" > io.csv
-sadf -T -- -b sar.out |tr "\t" , >> io.csv
+echo "\"timestamp\",\"IO\",\"type\",\"value\"" > io.csv
+sadf -T -- -b sar.out |awk '{print $3"T"$4"Z,"$5","$6","$7}' >> io.csv
